c6a87b18b7892241470a6bcb6bcec4bd1f180518
Kunal Agrawal <66416646+kunalagra@users.noreply.github.com>
2023-11-10 23:13:58 +0530

    add: Create new folder for the audiobook

--- m4bsplit.py
+++ m4bsplit.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 
-import sys, re, ffmpeg
+import sys, re, ffmpeg, os
 
 try:
     filename = sys.argv[1]
-    bookFile = ffmpeg.input(filename)
-    metaDict = ffmpeg.probe(filename,show_chapters=None)
+    filepath = os.path.join(os.getcwd(), filename)
+    bookFile = ffmpeg.input(filepath, vn=None)
+
+    metaDict = ffmpeg.probe(filepath,show_chapters=None)
+    os.makedirs(filename.split('.')[0])
+    os.chdir(filename.split('.')[0])
+
 except IndexError:
     print("File name missing, must have file name passed as argument")
     sys.exit(1)
@@ -17,7 +22,7 @@ for i in range(0,len(metaDict['chapters']),1):
     endTime = metaDict['chapters'][i]['end_time']
     chapNum = metaDict['chapters'][i]['id'] + 1
 
-    trackName = "{} {}.mp3".format(chapNum, chapTitle)
+    trackName = "{}.mp3".format(chapTitle)
 
     outbound = ffmpeg.output(bookFile,trackName,ss=startTime,to=endTime,map_chapters="-1")
     ffmpeg.run(outbound)
